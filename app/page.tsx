879c0bcc7749ebff8242dd93daacd9685d283fae
Adam Ziff <adamhziff@gmail.com>
2024-11-27 20:35:12 -0500

    Set up NextJS repo, list projects on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,11 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
+        <h1>PROJECTS UNDER CONSTRUCTION:</h1>
+        <h1>1. .M3U8 TO SPOTIFY</h1>
+        <h1>2. PARTY PLANNER CHATBOT</h1>
+        <h1> - DJ ZIFF</h1>
+        {/* <Image
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
@@ -47,10 +51,10 @@ export default function Home() {
           >
             Read our docs
           </a>
-        </div>
+        </div> */}
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
+        {/* <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
@@ -94,7 +98,7 @@ export default function Home() {
             height={16}
           />
           Go to nextjs.org →
-        </a>
+        </a> */}
       </footer>
     </div>
   );
